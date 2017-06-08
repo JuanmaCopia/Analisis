9323746d3c4cfe5120ce5ead9b931bb26e0e7251
Juanma <jmcopia96@gmail.com>
2017-06-08 12:17:47 -0300

    comienzo con spark

--- trivia-app/src/main/java/trivia/App.java
+++ trivia-app/src/main/java/trivia/App.java
@@ -1,22 +1,53 @@
 package trivia;
+
 import org.javalite.activejdbc.Base;
+
 import trivia.User;
 
-/**
- * Hello world!
- *
- */
-public class App
-{
-    public static void main( String[] args )
-    {
-      Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
+import java.util.*;
+
+import static spark.Spark.*;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
+
+
+public class App {
+
+    public static void main( String[] args ) {
+
+    staticFileLocation("/public");
+
+
+   
+    get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<>();
+        return new ModelAndView(model, "./src/main/resources/templates/ppal.mustache"); 
+    }, new MustacheTemplateEngine());
+
+    post("/registrarse", (req,res) -> {
+      Map<String, Object> model = new HashMap();
+      return new ModelAndView(model, "./src/main/resources/templates/registro.mustache");
+    },new MustacheTemplateEngine());
+
+    post("/ingresar", (req,res) -> {
+      Map<String, Object> model = new HashMap();
+      return new ModelAndView(model, "./src/main/resources/templates/ingreso.mustache");
+    },new MustacheTemplateEngine());
+
+
+
+
+
+    /*
+	Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/trivia", "root", "root");
 
       /*User u = new User();
       u.set("username", "root");
       u.set("password", "root");
       u.saveIt();
-*/
+
       User s = new User();
       s.set("username", "asdaaaahaassssjj");
       s.set("password", "juanma");
@@ -29,5 +60,8 @@ public class App
       //Game g = s.playGame();
 
       Base.close();
+	*/
+
+
     }
 }
