7fff925d9a823f61708977bafbe7e59ccb20afad
Juanma <jmcopia96@gmail.com>
2017-11-03 19:29:48 -0300

    matches table modified

--- trivia-app/src/migrations/20171013163300_create_matches_table.sql
+++ trivia-app/src/migrations/20171013163300_create_matches_table.sql
@@ -2,6 +2,8 @@ CREATE TABLE matches (
   id  INT(11) NOT NULL auto_increment PRIMARY KEY,
   user1Id INT(11),
   user2Id INT(11),
+  user1Score INT(11),
+  user2Score INT(11),
   winnerId INT(11),
   state enum('Game_Over','Game_In_Progress'),
   created_at DATETIME,
